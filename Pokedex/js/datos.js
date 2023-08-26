f24df7643fe863eae80877e03d52b05bb5039956
AngeLSanchez210 <is21001@ues.edu.sv>
2023-08-26 09:32:09 -0600

    STATS

--- Pokedex/js/datos.js
+++ Pokedex/js/datos.js
@@ -130,24 +130,109 @@ const Pokedex = (function () {
   function mostrarDetalles(pokemon, tipo, color) {
     const infoPoke = `
       <div style="background-color: ${color};">
-        <img src="${pokemon.sprites.versions['generation-v']['black-white'].animated.front_default}" alt="${pokemon.name}" id="imgpokemon">
+        <img src="${pokemon.sprites.versions['generation-v']['black-white'].animated.front_default}" alt="${pokemon.name}" id="imgpokemon" style="max-width: 100%; height: auto;">
       </div>
-      <h2>${pokemon.name}</h2>
+      <h2 style="margin-top: 10px;">${pokemon.name}</h2>
       <p>Altura: ${(pokemon.height) / 10} m</p>
       <p>Peso: ${pokemon.weight / 10} kg</p>
       <p>Tipo: ${tipo}</p>
-      <button id="btnCerrar" class="btnCerrar">Cerrar</button>
+      <div id="menu" style="margin-top: 10px;">
+        <button id="btnCerrar" class="btnCerrar" style="background-color: #333; color: white; padding: 5px 10px; border: none; cursor: pointer;">Cerrar</button>
+        <button id="opcion1" class="opcion" style=" color: white; padding: 5px 10px; border: none; cursor: pointer;">Stats</button>
+        <button id="opcion2" class="opcion" style=" color: white; padding: 5px 10px; border: none; cursor: pointer;">Moves</button>
+      </div>
+      <div id="infoContainer" style="display: flex; justify-content: space-between; margin-top: 20px;">
+        <div id="Info1" style="flex: 1; padding: 10px;"></div>
+        <div id="Info2" style="flex: 1; padding: 10px;"></div>
+      </div>
     `;
-
+  
     detallesPoke.innerHTML = infoPoke;
     detallesPoke.style.right = '0';
-
+  
+  
     const btnCerrar = detallesPoke.querySelector('.btnCerrar');
     btnCerrar.addEventListener('click', () => {
       CerrarDetalles();
-      efectoBlur()
+      efectoBlur();
     });
-    efectoBlur()
+  
+    const opcion1 = detallesPoke.querySelector('#opcion1');
+    const opcion2 = detallesPoke.querySelector('#opcion2'); // defino variables para el boton y divs
+    const InfoDiv1 = document.querySelector('#Info1');
+    const InfoDiv2 = document.querySelector('#Info2');
+  
+    opcion1.addEventListener('click', () => {
+      mostrarOpcion1Info(InfoDiv1, pokemon); //creo el evento del  boton click y le paso un metodo que este mostrara la info de stats
+      InfoDiv2.innerHTML = '';  //recibe dos parametros
+    });
+  
+    opcion2.addEventListener('click', () => {
+      mostrarOpcion2Info(InfoDiv2, pokemon); //lo mismo para el metodo de moves
+      InfoDiv1.innerHTML = ''; 
+    });
+  
+    efectoBlur();
+  }
+  
+  
+  function mostrarOpcion1Info(infoDiv, pokemon) {
+    //creo una funcion, luego una variable para obtener las stats 
+    const stats = pokemon.stats;
+    const hp = stats.find(stat => stat.stat.name === 'hp').base_stat;  //con find obtenemos los valores individuales de cada estadistica
+    const attack = stats.find(stat => stat.stat.name === 'attack').base_stat;
+    const defense = stats.find(stat => stat.stat.name === 'defense').base_stat;
+    const spAttack = stats.find(stat => stat.stat.name === 'special-attack').base_stat;
+    const spDefense = stats.find(stat => stat.stat.name === 'special-defense').base_stat;
+    const speed = stats.find(stat => stat.stat.name === 'speed').base_stat;
+    const total = stats.reduce((acu, stat) => acu + stat.base_stat, 0); //luego utilizamos reduce para obtener la info almacenada en base_Stat para sumarla
+  
+    const info = `
+  <div class="info-stat">
+    <h3 style="text-align:center;padding:15px;" >ESTADISTICAS</h3>
+    <div class="stat-line">
+      <span class="stat-label">HP:</span>
+      <span class="stat-value">${hp}</span>
+      <div class="stat-bar hp-bar" style="--hp: ${hp}px;"></div>
+    </div>
+    <div class="stat-line">
+      <span class="stat-label">Ataque:</span>
+      <span class="stat-value">${attack}</span>
+      <div class="stat-bar attack-bar" style="--attack: ${attack}px;"></div>
+    </div>
+    
+    <div class="stat-line">  
+      <span class="stat-label">Defensa:</span>
+      <span class="stat-value">${defense}</span>
+      <div class="stat-bar defense-bar" style="--defense: ${defense}px;"></div>
+    </div>
+    <div class="stat-line">
+      <span class="stat-label">Ataque Especial:</span>
+      <span class="stat-value">${spAttack}</span>
+      <div class="stat-bar spAttack-bar" style="--spAttack: ${spAttack}px;"></div>
+    </div>
+    <div class="stat-line">
+      <span class="stat-label">Defensa Especial:</span>
+      <span class="stat-value">${spDefense}</span>
+      <div class="stat-bar spDefense-bar" style="--spDefense: ${spDefense}px;"></div>
+    </div>
+    <div class="stat-line">
+      <span class="stat-label">Velocidad:</span>
+      <span class="stat-value">${speed}</span>
+      <div class="stat-bar speed-bar" style="--speed: ${speed}px;"></div>
+    </div>
+    <div class="stat-line">
+      <span style="margin-rignt:3px;" class="stat-label">Total:</span>
+      <span class="stat-value">${total}</span>
+      <div class="stat-bar total-bar" style="--total: ${total}px;"></div>
+    </div>
+  </div>
+`;
+
+infoDiv.innerHTML = info;
+
+
+  
   }
 
   function CerrarDetalles() {
