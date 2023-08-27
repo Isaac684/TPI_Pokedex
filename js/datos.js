b778cca00d239283696ab04a69def96199cddbb0
DIGP1 <diegogarci098@gmail.com>
2023-08-27 17:17:37 -0600

    Se creo la funcion de busqueda

--- js/datos.js
+++ js/datos.js
@@ -11,21 +11,36 @@ const Pokedex = (function () {
     const data = await response.json();
     return data.results;
   }
-
   async function cartaPokemon() {
 
     const dataPokemon = await cargarDatos();
 
     dataPokemon.forEach(async pokemon => {
+      contenedorPoke.innerHTML = "";
       createPokeCard(pokemon, contenedorPoke, detallesPoke);
     });
+    //Se llama al input que contendra la informacion de busqueda
+    const txtBusqueda = document.getElementById("txtBuscar");
+    txtBusqueda.addEventListener("input", () =>{ //Se le asigna el evento input para que cuando el texto cambie se ejecute la busqueda
+      const resultado = pokeSearch(txtBusqueda.value, dataPokemon);//Se le mandan los datos a la funcion pokeSearch para que este nos devuelva un arreglo con los elementos encontrados
+      if(resultado.length >0){//Se verifica si hay elementos
+        contenedorPoke.innerHTML = "";
+        //En el caso de que hayan elementos de recorren y se crean sus cartas para que sean mostradas
+        resultado.forEach(async pokemonesEncontrados =>{
+          createPokeCard(pokemonesEncontrados,contenedorPoke,detallesPoke);
+        })
+      }else{
+        //En el caso de que no entonces simplemente se muestra un texto indicando que no se encontro nada
+        contenedorPoke.innerHTML = "";
+        const fallo = document.createElement("p");
+        fallo.textContent = "No se encontro a ningun pokemon :(";
+
+        contenedorPoke.appendChild(fallo);
+      }
+    });
+
 
   }
-
-  
-
-
-
   document.getElementById('introVideo').addEventListener('ended', function () {
     document.getElementById('intro').classList.toggle("introSalida")
     document.body.style.overflow = 'auto';
--- js/pokeSearch.js
+++ js/pokeSearch.js
@@ -1,4 +1,7 @@
 export function pokeSearch(namePokemon,dataPokemon){
-    
+    //Se buscan los pokemones en el arreglo y los que vayan coincidiendo se guardan en un arreglo y se retona
+    const resultado = dataPokemon.filter(pokemon=> pokemon.name.includes(namePokemon));
+
+    return resultado;
 }
 
